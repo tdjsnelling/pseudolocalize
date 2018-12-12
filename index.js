d85b7230f671f2ff72ed0d409ad04b27bbb1f6ad
Tom Snelling <tomsnelling8@gmail.com>
2018-12-12 16:47:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const fs = require('fs')
+const psuedolocalize = require('./psuedolocalize')
+
+let localizedData = {}
+
+fs.readFile(process.argv[2], (err, data) => {
+  if (err) throw err
+  localizeFile(JSON.parse(data), null)
+})
+
+localizeFile = (data, parent) => {
+  const keys = Object.keys(data)
+  for (i in keys) {
+    const key = keys[i]
+    const value = data[keys[i]]
+    if (typeof value === 'string') {
+      if (!parent) {
+        localizedData[key] = psuedolocalize(value)
+      }
+      else {
+        if (!localizedData[parent]) {
+          localizedData[parent] = {}
+        }
+        localizedData[parent][key] = psuedolocalize(value)
+      }
+      if (value.includes('{{') && value.includes('}}')) {
+        const stringWithoutVariables = value.split(new RegExp(/{{.*?}}/gm))
+        if (stringWithoutVariables.length === 2) {
+            let localizedStringWithoutVariables = []
+            for (j in stringWithoutVariables) {
+                localizedStringWithoutVariables.push(psuedolocalize(stringWithoutVariables[j]))
+            }
+            localizedData[key] = localizedStringWithoutVariables.join(value.match(new RegExp(/{{.*?}}/gm)))
+        }
+      }
+    }
+    else if (typeof value === 'object' && !Array.isArray(value)) {
+      localizeFile(value, key)
+    }
+    else if (Array.isArray(value)) {
+      let localizedArray = []
+      for (j in value) {
+        localizedArray.push(psuedolocalize(value[j]))
+      }
+      localizedData[key] = localizedArray
+    }
+    else {
+      localizedData[key] = value
+    }
+  }
+  fs.writeFile(`localized-${process.argv[2]}`, JSON.stringify(localizedData, null, 2), err => {
+    if (err) throw err
+  })
+}
